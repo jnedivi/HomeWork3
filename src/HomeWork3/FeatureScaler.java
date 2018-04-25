db4f81d41c818368dd789690db2088cc691e9ce4
jnedivi <jnedivi@gmail.com>
2018-04-25 13:32:49 +0300

    Finished FeatureScaler

--- src/HomeWork3/FeatureScaler.java
+++ src/HomeWork3/FeatureScaler.java
@@ -2,6 +2,9 @@ package HomeWork3;
 
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Normalize;
+import weka.filters.unsupervised.attribute.Standardize;
 
 public class FeatureScaler {
 	/**
@@ -9,7 +12,14 @@ public class FeatureScaler {
 	 * @param instances The original dataset.
 	 * @return A scaled instances object.
 	 */
-	public Instances scaleData(Instances instances) {
-		return null;
+	public Instances scaleData(Instances instances) throws Exception {
+		
+		Instances scaledData = new Instances(instances);
+		Filter standardize = new Standardize();
+		
+		standardize.setInputFormat(scaledData);
+		scaledData = Filter.useFilter(scaledData, standardize);
+		
+		return scaledData;
 	}
 }

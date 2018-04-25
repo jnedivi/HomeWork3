bbc46e653e252712ca13bf6567e4e2a1f8d98b0e
Tal <Tal1414@gmail.com>
2018-04-25 17:05:22 +0300

    nnn

---
+++ src/HomeWork3/FeatureScaler.java
@@ -0,0 +1,24 @@
+package HomeWork3;
+
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Standardize;
+
+public class FeatureScaler {
+	/**
+	 * Returns a scaled version (using standarized normalization) of the given dataset.
+	 * @param instances The original dataset.
+	 * @return A scaled instances object.
+	 */
+	public Instances scaleData(Instances instances) throws Exception {
+		
+		Instances scaledData = new Instances(instances);
+		Filter standardize = new Standardize();
+		
+		standardize.setInputFormat(scaledData);
+		scaledData = Filter.useFilter(scaledData, standardize);
+		
+		return scaledData;
+	}
+}
